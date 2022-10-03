d1adfd3aa06d8f192fa44f8acbcec6b835801fa6
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-03 09:24:50 +0200

    update searchfilter in navbar

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import SearchIcon from '@mui/icons-material/Search';
 
 const url = "https://example-data.draftbit.com/books?_limit=200";
 
 const Books = () => {
 const [search, setSearch] = useState("") 
 const [book, setBook] = useState([])
+const [filteredData, setFilteredData] = useState([])
 
 useEffect(() =>{ 
     axios.get(url).then((response) => {
@@ -13,9 +15,39 @@ useEffect(() =>{
         console.log((response.data))
     })
 }, []); 
+
+ const handleFilter = (e) =>{
+    const searchWord = e.target.value;
+    const newFilter = book.filter((value)=>{
+      return value.title.toLowerCase().includes(searchWord)
+    })
+    {searchWord === "" ? setFilteredData([]) : setFilteredData(newFilter)}
+ }
+
+  const handleShow = (e) =>{
+    setSearch(e.target.value)
+  } 
+
+  const handleSearch = e => {
+    console.log(e.target)
+    handleShow(e);
+    handleFilter(e);
+  }
+
   return (
     <div className='main-container'>
-        <input type="text" placeholder='Search...' onChange={(e) => setSearch(e.target.value)}/>
+      <div className="search">
+       <div className='search-content'>
+        <input type="text" placeholder='Search...' onChange={handleSearch}/>
+        <div className='search-icon'> <SearchIcon /></div>
+        {filteredData.length != 0 && (
+          <div className='dataResult'> {filteredData.slice(0, 10).map((item)=>(
+            <a href="dataItem"><p>{item.title}</p></a>
+          ))}
+          </div>
+        )}
+       </div>
+      </div>  
       {book.filter((item)=>{
         return search.toLowerCase() === "" ? item : item.title.toLowerCase().includes(search)
       }).map((item) =>(
