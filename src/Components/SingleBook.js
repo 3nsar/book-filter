234f67f09ba62d4fd234718f157d8f3ec77b3599
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-12 13:28:12 +0200

    SingleBook update added more info

--- src/Components/SingleBook.js
+++ src/Components/SingleBook.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom'
+import StarIcon from '@mui/icons-material/Star';
+import StarHalfIcon from '@mui/icons-material/StarHalf';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
 
 
 const SingleBook = () => {
@@ -22,7 +25,24 @@ if(singleBook){
     return(
         <div>
             <h1>{singleBook.title}</h1>
-            <h2>{singleBook.id}</h2>
+            <h2>{singleBook.authors}</h2>
+            <img src={singleBook.image_url} alt="pic" />
+           
+            <div className='book-desc'>
+            <p>{singleBook.description}</p>
+            </div>
+
+            <p>Edition: {singleBook.edition}</p>
+            <p>Genres: {singleBook.genres}</p>
+            <p>Pages: {singleBook.num_pages}</p>
+            <p>{singleBook.rating >= 4.1 ? <> <StarIcon/> <StarIcon/> <StarIcon/> <StarIcon/> <StarIcon/> </> 
+                    : singleBook.rating >= 4 ? <> <StarIcon/> <StarIcon/> <StarIcon/> <StarIcon/> <StarHalfIcon/> </> 
+                    : singleBook.rating >= 3.9 ? <> <StarIcon/> <StarIcon/> <StarIcon/> <StarIcon/> <StarBorderIcon /> </> 
+                    : singleBook.rating >= 3.8 ? <> <StarIcon/> <StarIcon/> <StarIcon/> <StarHalfIcon/>  <StarBorderIcon /></> 
+                    : singleBook.rating >= 3.7 ? <> <StarIcon/> <StarIcon/> <StarIcon/> <StarBorderIcon /> <StarBorderIcon /></> 
+                    : singleBook.rating >= 3.55 ? <> <StarIcon/> <StarIcon/> <StarHalfIcon/> <StarBorderIcon /> <StarBorderIcon /></> 
+                    : <><StarIcon/>  <StarIcon/> <StarBorderIcon /> <StarBorderIcon /> <StarBorderIcon /></>} </p>
+            <p>({singleBook.rating})</p>
             <Link to={`/`}>go back</Link>
         </div>
     )
