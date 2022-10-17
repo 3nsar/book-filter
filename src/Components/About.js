e7424d28e3d344342578b1b07e19cdc13d890925
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-17 22:14:47 +0200

    about comp update

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,8 +3,12 @@ import booksImg from "./booksImg.svg"
 
 const About = () => {
   return (
-    <div>
-        <img src={booksImg} alt="pic" />
+    <div className='about-container'>
+        <div className="img">
+        <img src={booksImg} alt="pic" /></div>
+         <div>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima consequatur, sunt ut, fugit delectus saepe aliquam expedita commodi cum temporibus amet officia ill</p>
+         </div>
     </div>
   )
 }
