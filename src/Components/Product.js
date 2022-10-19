2a8ffd20187a97722bb24fdba316dc92d84c2df5
3nsar <84131267+3nsar@users.noreply.github.com>
2022-10-19 23:26:58 +0200

    prod comp update

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -3,12 +3,14 @@ import React from 'react'
 const Product = (props) => {
   return (
     <div className='product-container'>
+      <div className="product-content">
         <img src={props.image} alt="pic" />
         <p className='card-title'>{props.title}</p>
         <p className='card-authors'>{props.authors}</p>
         <div className='card-rating'>
         <p>{props.ratingStars} </p>
         <p>({props.rating})</p>
+        </div>
         </div>
     </div>
   )
